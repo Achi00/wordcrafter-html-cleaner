74f37d991035f7780cb630f7c54127ae980a571a
unknown <toolvlg@gmail.com>
2024-01-24 12:31:37 +0400

    adding gpt-3.5 summarizer

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import asyncio
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
+from openai import AsyncOpenAI
+
+client = AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 app = Flask(__name__)
 
@@ -29,12 +36,36 @@ def process_html():
     # Remove empty sections and the 'General' section if it's empty
     unwanted_titles = [""]  # Add any other unwanted titles here
     structured_data = {k: v for k, v in structured_data.items() if v and k not in unwanted_titles}
+    # Summarize the content of each section
+    # summarized_data = {}
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
+    # summarized_data = loop.run_until_complete(get_openai_summary(structured_data))
+    # loop.close()
 
     response = {
-        'structured_data': structured_data
+        'structured_data': structured_data,
+        # 'summarized_data': summarized_data
     }
 
     return jsonify(response)
+
+# summatize with ai
+async def get_openai_summary(structured_data):
+    summarized_data = {}
+    for heading, texts in structured_data.items():
+        full_text = ' '.join(texts)
+        chat_completion = await client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant"},
+                    {"role": "user", "content": f"Classify the following text into a category such as general, technical, or code in one word: {full_text}"}
+            ],
+            model="gpt-3.5-turbo-1106",
+        )
+        summary = chat_completion.choices[0].message.content
+        summarized_data[heading] = summary
+
+    return summarized_data
 
 if __name__ == '__main__':
     app.run(debug=True)
